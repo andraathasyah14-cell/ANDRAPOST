1c3b6adda7c588d28b95486e6779904b473ee66f
Andra Athasyah <andraathasyah14@gmail.com>
2025-09-17 05:59:34 +0000

    File changes

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -6,7 +6,6 @@ import { getStorage } from 'firebase/storage';
 import { getDatabase } from 'firebase/database';
 
 // Your web app's Firebase configuration from environment variables
-// Make sure to create a .env.local file with these values
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
