a951832d99183b43d40221b00b82733f965ff710
Andra Athasyah <andraathasyah14@gmail.com>
2025-09-17 02:32:03 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/components/admin/profile-form.tsx
+++ src/components/admin/profile-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useActionState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { useEffect, useRef } from 'react';
 import { useForm, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
